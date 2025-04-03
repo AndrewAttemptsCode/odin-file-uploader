395058f459a5b5db501b1f44c1c6912fa7b55ed4
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-03 16:33:42 +0100

    feat: add express session that uses prisma as a session store

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const expressSession = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
 
 const app = express();
 
@@ -8,6 +11,25 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  expressSession({
+    cookie: {
+     maxAge: 7 * 24 * 60 * 60 * 1000
+    },
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(
+      new PrismaClient(),
+      {
+        checkPeriod: 2 * 60 * 1000,
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }
+    )
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('test test 123');
