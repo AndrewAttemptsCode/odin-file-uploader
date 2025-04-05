6f92675628c84b9605b9521f0a38a51e21ba9225
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-05 15:08:37 +0100

    feat: add logic for the get and post register account route with error form validation handling

---
+++ src/controllers/authController.js
@@ -0,0 +1,47 @@
+const prisma = require("../../config/prisma");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const { validationResult } = require("express-validator");
+
+const getRegister = (req, res) => {
+  res.render("registerform", { title: "Register an Account" });
+};
+
+const postRegister = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const allErrors = errors.array();
+    const usernameError = allErrors?.find((error) => error.path === "username")?.msg;
+    const emailError = allErrors?.find((error) => error.path === "email")?.msg;
+    const passwordError = allErrors?.find((error) => error.path === "password")?.msg;
+    const confirmPasswordError = allErrors?.find((error) => error.path === 'confirmPassword')?.msg;
+    
+    return res.status(400).render("registerform", {
+      title: "Register an Account",
+      email,
+      username,
+      password,
+      usernameError,
+      emailError,
+      passwordError,
+      confirmPasswordError,
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await prisma.user.create({
+    data: {
+      email,
+      username,
+      password: hashedPassword,
+    },
+  });
+
+  res.redirect('/auth/login');
+
+});
+
+module.exports = { getRegister, postRegister };
