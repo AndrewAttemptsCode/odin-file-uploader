ca829efb19a068a4cb51cdbd493f3ac273e7fd99
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-05 15:07:31 +0100

    feat: add auth register route to get the register view and post register form

---
+++ src/routes/authRoute.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const authController = require('../controllers/authController');
+const registerValidation = require('../validators/registerValidation');
+
+const authRoute = Router();
+
+authRoute.get('/register', authController.getRegister);
+authRoute.post('/register', registerValidation, authController.postRegister);
+
+module.exports = authRoute;
