19b86867963b99491c12aabda3eaa346b6bc7f2b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-06 19:58:04 +0100

    feat: add form and functionality to upload a single file

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -2,4 +2,16 @@ const getIndex = (req, res) => {
   res.render('index', { title: 'File Manager', user: req.user });
 }
 
-module.exports = { getIndex };
+const getUpload = (req, res) => {
+  if (!req.user) {
+    return res.redirect('/auth/login');
+  }
+  res.render('uploadform', { title: 'Upload a file', user: req.user });
+}
+
+const postUpload = (req, res) => {
+  console.log(req.file);
+  res.send('File upload successfully');
+}
+
+module.exports = { getIndex, getUpload, postUpload };
--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
 const indexController = require('../controllers/indexController');
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 const indexRoute = Router();
 
 indexRoute.get('/', indexController.getIndex);
+indexRoute.get('/upload', indexController.getUpload);
+indexRoute.post('/upload', upload.single('fileupload'), indexController.postUpload);
 
 module.exports = indexRoute;
