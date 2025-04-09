efe54ea4026102590ea56fb0b6708f28de741164
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-09 17:41:52 +0100

    feat: add function to display the current file selected for input type file

--- public/script.js
+++ public/script.js
@@ -8,3 +8,14 @@ const toggleEdit = () => {
     input.select();
   }
 }
+
+const displayFile = () => {
+  const fileInput = document.querySelector('#fileupload');
+  const fileNameDisplay = document.querySelector('.file-name');
+
+  if (fileInput.files[0]) {
+    fileNameDisplay.textContent = fileInput.files[0].name;
+  } else {
+    fileNameDisplay.textContent = 'No file chosen';
+  }
+}
