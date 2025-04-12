dfa519ea7535af6074fde2fe7135e73d0234fc37
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-12 16:19:16 +0100

    feat: add folder form naming validation middleware

--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -9,7 +9,7 @@ const indexRoute = Router();
 indexRoute.get('/', indexController.getIndex);
 indexRoute.get('/upload', indexController.getUpload);
 indexRoute.post('/folder/:folderId', upload.single('fileupload'), indexController.postUpload);
-indexRoute.post('/create-folder', indexController.postFolder);
+indexRoute.post('/create-folder', folderNameValidation, indexController.postFolder);
 indexRoute.get('/folder/:folderId', indexController.getFolder);
 indexRoute.post('/folder/:folderId/update-folder', folderNameValidation, indexController.updateFolder);
 indexRoute.get('/folder/:folderId/delete-folder', indexController.deleteFolder);
