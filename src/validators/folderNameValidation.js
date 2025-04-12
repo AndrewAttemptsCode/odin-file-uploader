54446f38908f5995fffcaa3c803d07870322a156
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-04-12 15:49:45 +0100

    refactor: rename folder validation to be general descriptive for reuse

--- src/validators/folderNameValidation.js
+++ src/validators/folderNameValidation.js
@@ -1,10 +1,10 @@
 const { body } = require('express-validator');
 
-const editFolderNameValidation = [
+const folderNameValidation = [
   body('folderName')
     .trim()
     .notEmpty().withMessage('Folder name is empty.')
     .isLength({ max: 20 }).withMessage('Folder name exceeds maximum character length of 20')
 ];
 
-module.exports = editFolderNameValidation;
+module.exports = folderNameValidation;
